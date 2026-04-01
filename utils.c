f5f6d05d3a04e5ee4853eaffa87e9e25dd75fe8f
Bohdan Misonh <46958633+raikoho@users.noreply.github.com>
2026-04-01 10:27:35 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -3,8 +3,17 @@
 #include <elf.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "utils.h"
+
+// AArch64 register structure (define it ourselves for portability)
+struct user_pt_regs {
+    unsigned long regs[31];
+    unsigned long sp;
+    unsigned long pc;
+    unsigned long pstate;
+};
 
 cpu_state_t get_cpu_state(pid_t pid) {
     struct iovec iov;
@@ -12,11 +21,14 @@ cpu_state_t get_cpu_state(pid_t pid) {
 
     cpu_state_t state = {0};
 
+    memset(&regs, 0, sizeof(regs));
+
     iov.iov_base = &regs;
     iov.iov_len = sizeof(regs);
 
     if (ptrace(PTRACE_GETREGSET, pid, (void*)NT_PRSTATUS, &iov) == -1) {
         perror("ptrace GETREGSET failed");
+        exit(1);
     }
 
     state.pc = regs.pc;
