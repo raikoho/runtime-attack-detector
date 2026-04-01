2c5852eb391bd1f562fa1070512be76af426204b
Bohdan Misonh <46958633+raikoho@users.noreply.github.com>
2026-04-01 10:15:28 +0200

    Create return_detector.c

---
+++ modules/return_detector.c
@@ -0,0 +1,19 @@
+#include "../detector.h"
+
+int is_valid_code(uint64_t addr) {
+    return (addr >= 0x400000 && addr <= 0x500000); // simplified
+}
+
+int return_check(cpu_state_t *state) {
+    if (!is_valid_code(state->pc)) {
+        return 1;
+    }
+    return 0;
+}
+
+detector_module_t return_module = {
+    .name = "Invalid Return Address",
+    .init = NULL,
+    .check = return_check,
+    .cleanup = NULL
+};
