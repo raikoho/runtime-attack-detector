3d44457871434b954bd3027d8703081e0c4d6bb1
Bohdan Misonh <46958633+raikoho@users.noreply.github.com>
2026-04-01 10:21:55 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,19 +1,27 @@
 #include <sys/ptrace.h>
-#include <sys/user.h>
+#include <sys/uio.h>
+#include <elf.h>
 #include <stdio.h>
+#include <string.h>
+
 #include "utils.h"
 
 cpu_state_t get_cpu_state(pid_t pid) {
+    struct iovec iov;
     struct user_pt_regs regs;
+
     cpu_state_t state = {0};
 
-    if (ptrace(PTRACE_GETREGSET, pid, NT_PRSTATUS, &regs) == -1) {
+    iov.iov_base = &regs;
+    iov.iov_len = sizeof(regs);
+
+    if (ptrace(PTRACE_GETREGSET, pid, (void*)NT_PRSTATUS, &iov) == -1) {
         perror("ptrace GETREGSET failed");
     }
 
     state.pc = regs.pc;
     state.sp = regs.sp;
-    state.lr = regs.regs[30]; // LR = x30
+    state.lr = regs.regs[30]; // x30 = LR
 
     return state;
 }
